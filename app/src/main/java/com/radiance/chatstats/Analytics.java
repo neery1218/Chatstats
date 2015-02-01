06599d1e29fd198e01a40390a6034a5b97670f77
neery1218 <neery1218@gmail.com>
2015-01-31 21:55:37 -0500

    Found old analytics class, replaced current one

--- app/src/main/java/com/radiance/chatstats/Analytics.java
+++ app/src/main/java/com/radiance/chatstats/Analytics.java
@@ -1,36 +1,40 @@
 package com.radiance.chatstats;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import com.radiance.chatstats.SMS.Status;
 
 //Static Class
-public class Analytics {
+//Static Class
+public class Analytics {// returns statistics to the fragments for display
+
     private static ArrayList<String> commonWords;
     private static ArrayList<String> emoticons;
     private static ConversationThread c;
     private static final String [] EMOTICONS = {":p", ":)", ";)",":S"};
 
-    public Analytics (ConversationThread c){
+    public Analytics (ConversationThread c){// basic constructor, accepts the conversationThread
         this.c = c;
 
 
 
     }
-    public static ArrayList<StatPoint> getEmoticonCount (){
+    public static ArrayList<StatPoint> getEmoticonCount (){// returns an ArrayList of Statpoints signifying the number of each emoticon used
         ArrayList<StatPoint> temp = new ArrayList<StatPoint> ();
         for (int i = 0; i< EMOTICONS.length; i++){
             temp.add(searchFor(EMOTICONS[i]));
         }
         return temp;
     }
-    public static StatPoint searchFor (String s){
+    private static StatPoint searchFor (String s){// private method used to search the SMS messages for a passed string
         ArrayList<SMS> searchArray = c.getMessages();
         int S =0, R = 0;
 
         for (int i = 0; i < searchArray.size(); i++){
 
 
-            if (searchArray.get(i).getStatus() == Status.SENT)
+            if (searchArray.get(i).getStatus() == Status.SENT)// checks enum for type of message
                 S+=searchArray.get(i).getNumOf(s);
             else
                 R+=searchArray.get(i).getNumOf(s);
@@ -40,21 +44,21 @@ public class Analytics {
     }
 
     //\/\/\/\/\/\/\/\WHO'S WINNING/\/\/\/\/\/\/\/\/\/\/\/\/
-    public static StatPoint sentAndReceived()
-    {
+    public static StatPoint sentAndReceived()// returns amount of sent messages vs received
+    {// maybe responses instead?
         int S = c.getSent().size();
         int R = c.getReceived().size();
         return (new StatPoint(S, R));
     }
 
-    public static StatPoint getAvgMessageLengthWords() {
+    public static StatPoint getAvgMessageLengthWords() {// returns the average message length
         double S = 0.0, R = 0.0;
 
         double sentSize = 0, receivedSize = 0;
         Response temp;
         ArrayList<Response> responses = c.getResponses();
 
-        for (int i = 0; i < responses.size(); i++)
+        for (int i = 0; i < responses.size(); i++)// cycles through the responses
         {
             temp = responses.get(i);
             if (temp.getStatus() == Status.RECEIVED)
@@ -71,12 +75,12 @@ public class Analytics {
         }
 
         S /= sentSize;
-        R /= receivedSize;
+        R /= receivedSize;// maybe truncate it at some point? decimals mean nothing
 
         return (new StatPoint(S, R));
     }
 
-    public static StatPoint getInitiateCount ()
+    public static StatPoint getInitiateCount ()// returns the amount of initiations per user
     {
         ArrayList<Conversation> conversations = c.getConversations();
 
@@ -92,7 +96,7 @@ public class Analytics {
         return (new StatPoint(S,R));
     }
 
-    public static StatPoint getResponseTime ()
+    public static StatPoint getResponseTime ()// returns average response time
     {
         ArrayList<Response> temp;
         ArrayList<Conversation> conversations = c.getConversations();
@@ -104,15 +108,28 @@ public class Analytics {
 
         for (int i = 0; i < conversations.size(); i++)
         {
-            temp = conversations.get(i).getConversation();
-            for (int j = 0; j < temp.size()-1; j++)
+            temp = conversations.get(i).getConversation();// returns arrayList of responses. i know, confusing af
+            if (temp.get(0).getStatus() == Status.SENT)
+                responsesSent++;
+            else
+                responsesReceived++;
+            for (int j = 1; j < temp.size(); j++)
             {
-                //TODO fix averageResponseTime methodasdfadsfa
+                if(temp.get(j).getStatus() == Status.SENT){
+                    timeSent+=(temp.get(j).getDateStart()-temp.get(j-1).getDateEnd());
+                    responsesSent++;
+                }
+
+                else{
+                    timeReceived+=(temp.get(j).getDateStart()-temp.get(j-1).getDateEnd());
+                    responsesReceived++;
+                }
+
             }
         }
-
-        timeSent /= (responsesSent*60000);
-        timeReceived /= (responsesReceived*60000);
+        Log.v("Time", "" + timeSent + "    " + timeReceived);
+        timeSent /= (responsesSent*60000);//returns it in minutes
+        timeReceived /= (responsesReceived*60000);// responsesSent = sentSize, we're duplicating code here
 
         return new StatPoint(timeSent, timeReceived);
     }
