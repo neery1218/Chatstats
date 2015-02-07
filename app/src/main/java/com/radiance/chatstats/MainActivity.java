7c40c2510cad02aebe9ae344bcfe1b5f346d84c5
neery1218 <neery1218@gmail.com>
2015-02-07 18:00:49 -0500

    added gradle files switched all the stat stuff into StatsFragments so development of ContactsFragment can begin

--- app/src/main/java/com/radiance/chatstats/MainActivity.java
+++ app/src/main/java/com/radiance/chatstats/MainActivity.java
@@ -1,5 +1,7 @@
 package com.radiance.chatstats;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
@@ -8,63 +10,27 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
-    private Cursor rCursor, sCursor;
-    TextView text;
-    String address;
-    ConversationThread conversationThread;
-    ArrayList<Conversation> messages;
-    int i = 0;
+public class MainActivity extends ActionBarActivity implements StatsFragment.OnFragmentInteractionListener{
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {//pushing with
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //need to add fragments now
-        //set references
-        text = (TextView)findViewById((R.id.textView));
-       // text.setText("hi");
-        //get Contact address, any one
-// arbitrary position, just checking if messages work
-        // address =  cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        StatsFragment statsFragment = new StatsFragment();
+        fragmentTransaction.add(R.id.fragment_container,statsFragment);
+        fragmentTransaction.commit();
 
-        address = "+16479685968";
-        rCursor = getContentResolver().query(Uri.parse("content://sms/inbox"),new String [] {"address", "body", "date"},null,null,null);
-        sCursor = getContentResolver().query(Uri.parse("content://sms/sent"),new String [] {"address", "body", "date"},null,null,null);
-        conversationThread = new ConversationThread(rCursor,sCursor,address);
-        messages = conversationThread.getConversations();
-       Analytics analytics = new Analytics (conversationThread);
-       text.setText(""+ analytics.getResponseTime());
-        // mCursor.moveToFirst();
 
     }
 
-    public void displayMessage (View view){
-        //get address via itemClickListener
-
-
-        //get messages from address
-
-
-        //get inbox
-
-        //get sent
-
-        //merge and sort by unix time
-
-        //call stats
-
-        //messages = conversationThread.getMessages();
-        text.setText(messages.get(i).toString()+ "\n Date End:" + messages.get(i).getDateEnd() + "\n Date Start:"+messages.get(i).getDateStart() );
-        i++;
-        i%=messages.size();
-        //mCursor.moveToNext();
-
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -85,5 +51,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(){
+
     }
 }
