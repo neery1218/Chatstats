aa3b2900161b43e8b6b881fb50cd8b9f2b60859c
neery1218 <neery1218@gmail.com>
2015-02-11 21:59:53 -0500

    created ContactAdapter (subclass of ArrayAdapter), which takes in a Contact object (also created) and displays to a listview with a custom layout

---
+++ app/src/main/java/com/radiance/chatstats/ContactAdapter.java
@@ -0,0 +1,37 @@
+package com.radiance.chatstats;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Neerajen on 11/02/2015.
+ */
+public class ContactAdapter extends ArrayAdapter {
+    private Context context;
+    private ArrayList<Contact> contacts;
+
+    public ContactAdapter(Context context, ArrayList<Contact> contacts) {
+        super(context, R.layout.list_item, contacts);
+        this.contacts = contacts;
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.list_item, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.text1);
+
+        textView.setText(contacts.get(position).getName());
+
+
+        return rowView;
+    }
+}
