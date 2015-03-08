22e677b9ec73c6405cfc59987800fc5aa858b940
Richard Du <richarddu1226@gmail.com>
2015-03-08 18:59:26 -0400

    Finished Emoticon Statistics fragment.

--- app/src/main/java/com/radiance/chatstats/EmoticonsFragment.java
+++ app/src/main/java/com/radiance/chatstats/EmoticonsFragment.java
@@ -1,12 +1,15 @@
 package com.radiance.chatstats;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.RadarChart;
 import com.github.mikephil.charting.components.XAxis;
@@ -15,27 +18,19 @@ import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.RadarData;
 import com.github.mikephil.charting.data.RadarDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.ValueFormatter;
 
 import java.util.ArrayList;
 
 
 public class EmoticonsFragment extends Fragment {
     RadarChart radarChart;
-
+    ArrayList<StatPoint> emoticons;
 
     public EmoticonsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EmoticonsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static EmoticonsFragment newInstance(String param1, String param2) {
         EmoticonsFragment fragment = new EmoticonsFragment();
         return fragment;
@@ -44,45 +39,38 @@ public class EmoticonsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     public void setRadarChart() {
+        //Title
+        emoticons = StatsFragment.analytics.getEmoticonCount();
+        Log.d("TAG", emoticons.toString());
         radarChart.setDescription("");
 
-        radarChart.setWebLineWidth(1.5f);
-        radarChart.setWebLineWidthInner(0.75f);
-        radarChart.setWebAlpha(100);
+        //Defines properties of the web
+        radarChart.setWebLineWidth(2f);
+        radarChart.setWebLineWidthInner(2f);
+        radarChart.setWebAlpha(255);
+        radarChart.setWebColor(Color.parseColor("#FFFFFF"));
+        radarChart.setWebColorInner(Color.parseColor("#FFFFFF"));
 
-        /*// create a custom MarkerView (extend MarkerView) and specify the layout
-        // to use for it
-        MyMarkerView mv = new MyMarkerView(this, R.layout.custom_marker_view);
-
-        // set the marker to the chart
-        radarChart.setMarkerView(mv);*/
-
-
-        float mult = 150;
-        int cnt = 9;
+        int count = Analytics.EMOTICONS.length;
 
         ArrayList<Entry> yVals1 = new ArrayList<Entry>();
         ArrayList<Entry> yVals2 = new ArrayList<Entry>();
 
-        // IMPORTANT: In a PieChart, no values (Entry) should have the same
-        // xIndex (even if from different DataSets), since no values can be
-        // drawn above each other.
-        for (int i = 0; i < cnt; i++) {
-            yVals1.add(new Entry((float) (Math.random() * mult) + mult / 2, i));
+        for (int i = 0; i < count; i++) {
+            yVals1.add(new Entry((float) emoticons.get(i).getReceived(), i));
         }
 
-        for (int i = 0; i < cnt; i++) {
-            yVals2.add(new Entry((float) (Math.random() * mult) + mult / 2, i));
+        for (int i = 0; i < count; i++) {
+            yVals2.add(new Entry((float) emoticons.get(i).getSent(), i));
         }
 
         ArrayList<String> xVals = new ArrayList<String>();
 
-        for (int i = 0; i < cnt; i++)
-            xVals.add("a" + i);
+        for (int i = 0; i < count; i++)
+            xVals.add(Analytics.EMOTICONS[i]+"  ");
 
         RadarDataSet set1 = new RadarDataSet(yVals1, "Set 1");
         set1.setColor(ColorTemplate.VORDIPLOM_COLORS[0]);
@@ -98,36 +86,49 @@ public class EmoticonsFragment extends Fragment {
         sets.add(set1);
         sets.add(set2);
 
+
         RadarData data = new RadarData(xVals, sets);
-        data.setValueTypeface(MainActivity.typeFaceRegular);
-        data.setValueTextSize(8f);
+        data.setValueTypeface(MainActivity.oswaldLight);
+        data.setValueTextSize(16f);
         data.setDrawValues(false);
 
         radarChart.setData(data);
-
+        radarChart.getYAxis().setValueFormatter(
+                new ValueFormatter() {
+                    @Override
+                    public String getFormattedValue(float value) {
+                        return ""+(int)value;
+                    }
+                }
+        );
+        radarChart.getYAxis().setTextColor(Color.parseColor("#FFFFFF"));
         radarChart.invalidate();
 
         XAxis xAxis = radarChart.getXAxis();
         xAxis.setTypeface(MainActivity.typeFaceRegular);
-        xAxis.setTextSize(9f);
+        xAxis.setTextSize(16f);
+        xAxis.setTextColor(Color.parseColor("#FFFFFF"));
 
         YAxis yAxis = radarChart.getYAxis();
-        yAxis.setTypeface(MainActivity.typeFaceRegular);
+        yAxis.setTypeface(MainActivity.oswaldLight);
         yAxis.setLabelCount(5);
-        yAxis.setTextSize(9f);
+        yAxis.setTextSize(16f);
+        yAxis.setTextColor(Color.parseColor("#FFFFFF"));
         yAxis.setStartAtZero(true);
 
-        /*Legend l = radarChart.getLegend();
-        l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
-        l.setTypeface(MainActivity.typeFaceRegular);
-        l.setXEntrySpace(7f);
-        l.setYEntrySpace(5f);*/
+        radarChart.setRotationEnabled(false);
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_emoticons, container, false);
+
+        TextView numberEmoticons = (TextView) view.findViewById(R.id.numberEmoticons);
+        numberEmoticons.setTextColor(Color.WHITE);
+        numberEmoticons.setTypeface(MainActivity.oswaldLight);
+        numberEmoticons.setTextSize(48);
+
         radarChart = (RadarChart) view.findViewById(R.id.radarChart);
         setRadarChart();
 
