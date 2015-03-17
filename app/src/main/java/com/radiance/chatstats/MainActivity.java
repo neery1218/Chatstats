e8031b07f90d6839c9a0df4bea35e7fab005bd94
neery1218 <neery1218@gmail.com>
2015-03-17 00:01:46 -0400

    fixed some animation issues LoadingFragment is still crashing

--- app/src/main/java/com/radiance/chatstats/MainActivity.java
+++ app/src/main/java/com/radiance/chatstats/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends ActionBarActivity implements StatsFragment.OnT
     StatsFragment statsFragment;
     LoadingFragment loadingFragment;
     DialogFragment contactPhoneNumberDialog;
+    private Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -157,6 +158,24 @@ public class MainActivity extends ActionBarActivity implements StatsFragment.OnT
         fragmentTransaction.commit();
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.v("destroy", "destroy");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
+    }
     @Override
     public void onToBeDetermined(ArrayList<String> id) {
 
